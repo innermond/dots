489e59215e9d6dffccb5ef08468b8712f042deea
innermond <innermond@yahoo.com>
2023-01-20 13:20:13 +0200

    add .refresh for api container

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROm golang
 RUN mkdir /app
-ADD ./ /app
 WORKDIR /app
+COPY . .
 RUN go build -o ./cmd/dotsd/main ./cmd/dotsd/main.go
 EXPOSE 8080
 CMD ["/app/cmd/dotsd/main"]
--- cmd/dotsd/main.go
+++ cmd/dotsd/main.go
@@ -9,7 +9,7 @@ import (
 const ADDR = ":8080"
 
 func main() {
-	fmt.Println("server is starting")
+	fmt.Println("trying to start the server")
 	server := http.NewServer()
 	err := server.ListenAndServe(ADDR)
 	fmt.Println(err)
