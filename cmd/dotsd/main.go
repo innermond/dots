93a2a7d64a5a8080c1bb5f3381cb56658dffe340
innermond <innermond@yahoo.com>
2023-01-27 01:22:41 +0200

    add env + init securecookie

--- cmd/dotsd/main.go
+++ cmd/dotsd/main.go
@@ -9,11 +9,49 @@ import (
 
 	"github.com/innermond/dots/http"
 	"github.com/innermond/dots/postgres"
+	"github.com/joho/godotenv"
 )
 
 const addr = ":8080"
 
 func main() {
+	fmt.Println("initiating...")
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	clientId := os.Getenv("DOTS_GITHUB_CLIENT_ID")
+	clientSecret := os.Getenv("DOTS_GITHUB_CLIENT_SECRET")
+	if clientId == "" || clientSecret == "" {
+		log.Fatal("client credentials are missing")
+	}
+
+	dsn := os.Getenv("DOTS_DSN")
+	db := postgres.NewDB(dsn)
+	if err := db.Open(); err != nil {
+		panic(fmt.Errorf("cannot open database: %w", err))
+	}
+
+	server := http.NewServer()
+
+	err = server.OpenSecureCookie()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server.ClientID = clientId
+	server.ClientSecret = clientSecret
+
+	pingService := postgres.NewPingService(db)
+	authService := postgres.NewAuthService(db)
+	userService := postgres.NewUserService(db)
+
+	server.PingService = pingService
+	server.UserService = userService
+	server.AuthService = authService
+
 	ctx, cancel := context.WithCancel(context.Background())
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
@@ -21,20 +59,6 @@ func main() {
 		<-c
 		cancel()
 	}()
-
-	dsn := "postgresql://postgres:admin@127.0.0.1:5432/dots?sslmode=disable"
-	db := postgres.NewDB(dsn)
-	if err := db.Open(); err != nil {
-		panic(fmt.Errorf("cannot open database: %w", err))
-	}
-
-	pingService := postgres.NewPingService(db)
-	authService := postgres.NewAuthService(db)
-	userService := postgres.NewUserService(db)
-
-	server := http.NewServer()
-	server.PingService = pingService
-	server.UserService = userService
 
 	go func() {
 		fmt.Println("starting server...")
