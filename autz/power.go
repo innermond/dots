5c6c50a44ac948c9bcadadfadd54742169409d64
innermond <innermond@yahoo.com>
2023-04-02 02:22:59 +0300

    add powers

---
+++ autz/power.go
@@ -0,0 +1,77 @@
+package autz
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+type Power int
+
+// create powers
+const (
+	CreateOwn Power = iota
+	WriteOwn
+)
+
+var Powers = map[Power]string{
+	CreateOwn: "Can create its own items",
+	WriteOwn:  "Can edit its own items",
+}
+
+var ss = [...]string{"create_own", "write_own"}
+
+func (p Power) String() string {
+	if int(p) > len(ss)-1 {
+		return ""
+	}
+	return ss[p]
+}
+
+func (p Power) Bytes() []byte {
+	if int(p) > len(ss)-1 {
+		return nil
+	}
+	return []byte(ss[p])
+}
+
+func (p Power) Eq(other string) bool {
+	return p.String() == other
+}
+
+func (p Power) Description() string {
+	if desc, ok := Powers[p]; ok {
+		return desc
+	}
+	return ""
+}
+
+var PowerToManageOwn = []Power{CreateOwn, WriteOwn}
+
+func (p *Power) UnmarshalJSON(b []byte) error {
+	if string(b) == "null" || string(b) == `""` {
+		return nil
+	}
+
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+
+	for i, ps := range ss {
+		if s == ps {
+			*p = Power(i)
+			return nil
+		}
+	}
+
+	return errors.New("unmarshaling power")
+}
+
+func (p *Power) MarshalJSON() ([]byte, error) {
+	bb := p.Bytes()
+	if bb == nil {
+		return nil, errors.New("marshaling power")
+	}
+
+	return bb, nil
+}
