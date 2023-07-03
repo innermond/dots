8852d2161d601d762f368be46357921db72539a4
innermond <innermond@yahoo.com>
2023-07-03 22:12:42 +0300

    good bye trigger

---
+++ api/migrations/004_rmDeedTrigger.down.sql
@@ -0,0 +1,7 @@
+create or replace trigger update_drain_is_deleted_trigger after
+update
+    on
+    public.deed for each row
+    when ((old.deleted_at is distinct
+from
+    new.deleted_at)) execute function update_drain_is_deleted();
---
+++ api/migrations/004_rmDeedTrigger.up.sql
@@ -0,0 +1,1 @@
+drop trigger if exists update_drain_is_deleted_trigger on deed; 
