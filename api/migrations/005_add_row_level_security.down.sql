3ac07bf697cab1a2fdca299d6d33ee3bc5a1c8b9
innermond <innermond@yahoo.com>
2023-07-17 22:05:53 +0300

    disable rls

--- api/migrations/005_add_row_level_security.down.sql
+++ api/migrations/005_add_row_level_security.down.sql
@@ -11,3 +11,9 @@ alter table drain drop constraint drain_tid_fk_user_id;
 alter table entry drop column tid;
 alter table deed drop column tid;
 alter table drain drop column tid;
+
+alter table company disable row level security;
+alter table entry_type disable row level security;
+alter table entry disable row level security;
+alter table deed disable row level security;
+alter table drain disable row level security;
